8a3e5ded5f44e760c202b83fa4d6c97beab2fe2f
Your Name <you@example.com>
2026-08-05 15:21:38 -0700

    Creating a new user in Entra ID using PowerShell

---
+++ Add_Entra_Users.ps1
@@ -0,0 +1,13 @@
+# Creating Users in Entra ID using PowerShell
+
+# Prerequisites:
+# - Ensure you have the AzureAD or Microsoft Graph PowerShell module installed.
+# - You must have the necessary permissions to create users in Entra ID.
+# # Usage:
+# 1. Open PowerShell with administrative privileges.
+# 2. Run the script and provide the required user details as prompted.
+
+# Sign in to MgGraph
+##Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# Connect to Azure AD
