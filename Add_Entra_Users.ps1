c02ccda7368023fd5ad6fa09d1e52fca97a7c6ab
Your Name <you@example.com>
2026-08-05 18:18:22 -0700

    Added MgGraph and Entra.ID snippets

--- Add_Entra_Users.ps1
+++ Add_Entra_Users.ps1
@@ -7,7 +7,39 @@
 # 1. Open PowerShell with administrative privileges.
 # 2. Run the script and provide the required user details as prompted.
 
-# Sign in to MgGraph
+# Using MgGraph
 ##Connect-MgGraph -Scopes "User.ReadWrite.All"
+Install-Module Microsoft.Graph -Scope CurrentUser
 
-# Connect to Azure AD
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# 1. Define the temporary password profile
+$PasswordProfile = @{
+    Password = "SecurePassword123!"
+    ForceChangePasswordNextSignIn = $true
+}
+
+# 2. Create the new user
+New-MgUser -DisplayName "Jane Doe" `
+           -MailNickname "jdoe" `
+           -UserPrincipalName "jdoe@yourdomain.com" `
+           -AccountEnabled $true `
+           -PasswordProfile $PasswordProfile
+
+
+# Or using Microsoft.Entra ID module
+Install-Module Microsoft.Entra -Scope CurrentUser
+
+Connect-Entra -Scopes "User.ReadWrite.All"
+
+# 1. Define the password profile
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "SecurePassword123!"
+
+# 2. Create the new user
+New-EntraUser -DisplayName "Jane Doe" `
+              -MailNickname "jdoe" `
+              -UserPrincipalName "jdoe@yourdomain.com" `
+              -AccountEnabled $true `
+              -PasswordProfile $PasswordProfile
+
