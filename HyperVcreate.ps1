c372811dcddaf666980dd1a54c5156d5fc22b6ee
Your Name <you@example.com>
2026-08-08 20:16:45 -0400

    File Cleanup

--- HyperVcreate.ps1
+++ HyperVcreate.ps1
@@ -2,11 +2,11 @@
 # Use: Quickly spin up Hyper-V VMs for testing
 
 # Define Variables
-$VMName = "Virtual Machine Name"
+$VMName = "IntuneDemo"
 $VMPath = "C:\VMs\$VMName"
 $VHDPath = "C:\VMs\$VMName.vhdx"
 $SwitchName = "Default Switch"
-$VMISO = "PATH to ISO"
+$VMISO = "C:\VM ISOs\Win11.iso"
 
 # Create VM
 New-VM -Name $VMName `
@@ -29,3 +29,7 @@ New-VM -Name $VMName `
  Set-VMDvdDrive -VMName $VMName -Path $VMISO
  # Set first boot device to DVD
  Set-VMFirmware -VMName $VMName -FirstBootDevice $(Get-VMDvdDrive -VMName "$VMName")
+
+
+ #Optional: STart VM after creation
+ Start-VM -Name $VMName
